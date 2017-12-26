b5f778d6f186b6fbd700ad08d89dcaf11c4a4da8
liuzhenhua <liuzhenhuanet@gmail.com>
2017-12-26 18:34:55 +0800

    revert last commit

--- android/sdk/src/main/java/com/taobao/weex/ui/view/WXFrameLayout.java
+++ android/sdk/src/main/java/com/taobao/weex/ui/view/WXFrameLayout.java
@@ -21,21 +21,20 @@ package com.taobao.weex.ui.view;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.drawable.Drawable;
-import android.os.Build;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.view.MotionEvent;
 import android.widget.FrameLayout;
+
 import com.taobao.weex.ui.component.WXDiv;
 import com.taobao.weex.ui.flat.widget.Widget;
 import com.taobao.weex.ui.view.gesture.WXGesture;
 import com.taobao.weex.ui.view.gesture.WXGestureObservable;
 import com.taobao.weex.utils.WXLogUtils;
 import com.taobao.weex.utils.WXViewUtils;
+
 import java.lang.ref.WeakReference;
 import java.util.List;
-
-import static android.os.Build.VERSION.SDK_INT;
 
 /**
  * FrameLayout wrapper
@@ -51,9 +50,6 @@ public class WXFrameLayout extends FrameLayout implements WXGestureObservable,IR
 
   public WXFrameLayout(Context context) {
     super(context);
-    if (SDK_INT <= Build.VERSION_CODES.KITKAT) {
-      setLayerType(LAYER_TYPE_SOFTWARE, null);
-    }
   }
 
   @Override
--- android/sdk/src/main/java/com/taobao/weex/ui/view/WXImageView.java
+++ android/sdk/src/main/java/com/taobao/weex/ui/view/WXImageView.java
@@ -22,7 +22,6 @@ import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
-import android.os.Build;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.view.MotionEvent;
@@ -38,8 +37,6 @@ import com.taobao.weex.utils.WXLogUtils;
 import java.lang.ref.WeakReference;
 import java.util.Arrays;
 
-import static android.os.Build.VERSION.SDK_INT;
-
 public class WXImageView extends ImageView implements WXGestureObservable,
                                                       IRenderStatus<WXImage>,
                                                       IRenderResult<WXImage>, WXImage.Measurable {
@@ -51,9 +48,6 @@ public class WXImageView extends ImageView implements WXGestureObservable,
 
   public WXImageView(Context context) {
     super(context);
-    if (SDK_INT <= Build.VERSION_CODES.KITKAT) {
-      setLayerType(LAYER_TYPE_SOFTWARE, null);
-    }
   }
 
   @Override
